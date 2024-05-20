736be4c777e8322368ae8f30dd863abcaf037de2
RitteAnnti818 <heeyun29@icloud.com>
2024-05-20 11:55:50 +0900

    store_update(24.05.20)

--- src/panels/StorePanel.java
+++ src/panels/StorePanel.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.Graphics;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -14,15 +15,15 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
-public class StorePanel {
+public class StorePanel extends JPanel {
 
-    // 상점 버튼 이미지 아이콘 
-    private ImageIcon store = new ImageIcon("img/store/storePanel.png");
+	// 상점 버튼 이미지 아이콘
+	private ImageIcon storeIc = new ImageIcon("img/store/storeBtn.png");
 
-    private JButton StoreBtn;
+	private JButton StoreBtn;
 
-    public StorePanel(Object o) {
-        StoreBtn = new JButton(store);
+	public StorePanel(Object o) {
+		StoreBtn = new JButton(storeIc);
 		StoreBtn.setName("StoreBtn");
 		StoreBtn.addMouseListener((MouseListener) o);
 		StoreBtn.setBounds(254, 334, 291, 81);
@@ -30,5 +31,12 @@ public class StorePanel {
 		StoreBtn.setBorderPainted(false);
 		StoreBtn.setContentAreaFilled(false);
 		StoreBtn.setFocusPainted(false);
-    }
+	}
+
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g); // 화면을 비운다
+		
+		// 인트로 화면을 그린다
+		g.drawImage(storeIc.getImage(), -60, 0, /* this.getWidth(), this.getHeight(), */ null);
+	}
 }
