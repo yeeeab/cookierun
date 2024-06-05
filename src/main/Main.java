bba146a46b15042d2378eca3bf497d9f909ce36e
RitteAnnti818 <heeyun29@icloud.com>
2024-06-05 12:16:06 +0900

    240605_storeUpdate

--- src/main/Main.java
+++ src/main/Main.java
@@ -21,151 +21,138 @@ import potion.*;
 
 import java.awt.CardLayout;
 
-// windowBuilder 로 프레임만 제작하고 나머지는 입력
-
 public class Main extends listenAdapter {
 
-	private JFrame frame; // 창을 띄우기 위한 프레임
+    private JFrame frame; // 창을 띄우기 위한 프레임
+    private IntroPanel introPanel; // 인트로
+    private StorePanel storePanel; // 상점
+    private SelectPanel selectPanel; // 캐릭터 선택
+    private GamePanel gamePanel; // 게임진행
+    private SpeedrunPanel speedrunPanel;
+    private EndPanel endPanel; // 게임결과
+    private HealthPotion healthPotion;
+    private CardLayout cl; // 카드 레이이웃 오브젝트
+    private CookieImg ci; // 쿠키이미지
 
-	private IntroPanel introPanel; // 인트로
+    public GamePanel getGamePanel() {
+        return gamePanel;
+    }
 
-	private StorePanel storePanel; // 상점
+    public void setGamePanel(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
 
-	private SelectPanel selectPanel; // 캐릭터 선택
+    public SpeedrunPanel getSpeedrunPanel() {
+        return speedrunPanel;
+    }
 
-	private GamePanel gamePanel; // 게임진행
+    public EndPanel getEndPanel() {
+        return endPanel;
+    }
 
-	private SpeedrunPanel speedrunPanel;
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Main window = new Main();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	private EndPanel endPanel; // 게임결과
+    public Main() {
+        initialize();
+    }
 
-	private HealthPotion healthPotion;
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100, 100, 800, 500); // 창 사이즈 (100,100좌표는 아래의 frame.setLocationRelativeTo(null) 때문에 의미가 없어진다)
+        frame.setLocationRelativeTo(null); // 창을 화면 중앙에 배치
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 엑스버튼을 누르면 종료
+        cl = new CardLayout(0, 0); // 카드레이아웃 객체 생성
+        frame.getContentPane().setLayout(cl); // 프레임을 카드레이아웃으로 변경
 
-	private CardLayout cl; // 카드 레이이웃 오브젝트
+        introPanel = new IntroPanel(frame, cl);
+        introPanel.addMouseListener(this); // intro패널은 여기서 바로 넣는 방식으로 마우스리스너를 추가함.
 
-	private CookieImg ci; // 쿠키이미지
+        storePanel = new StorePanel(this); // 상점
+        selectPanel = new SelectPanel(this); // Main의 리스너를 넣기위한 this
+        gamePanel = new GamePanel(frame, cl, this); // Main의 프레임 및 카드레이아웃을 이용하고 리스너를 넣기위한 this
+        endPanel = new EndPanel(this); // Main의 리스너를 넣기위한 this
+        speedrunPanel = new SpeedrunPanel(frame, cl, this); // 스피드런 모드 패널 추가
 
-	public GamePanel getGamePanel() {
-		return gamePanel;
-	}
+        healthPotion = new HealthPotion();
 
-	public void setGamePanel(GamePanel gamePanel) {
-		this.gamePanel = gamePanel;
-	}
+        // 모든 패널의 레이아웃을 null로 변환
+        introPanel.setLayout(null);
+        storePanel.setLayout(null);
+        selectPanel.setLayout(null);
+        gamePanel.setLayout(null);
+        endPanel.setLayout(null);
 
-	public SpeedrunPanel getSpeedrunPanel() {
-		return speedrunPanel;
-	}
+        // 프레임에 패널들을 추가한다.(카드 레이아웃을 위한 패널들)
+        frame.getContentPane().add(introPanel, "intro");
+        frame.getContentPane().add(storePanel, "store");
+        frame.getContentPane().add(selectPanel, "select");
+        frame.getContentPane().add(gamePanel, "game");
+        frame.getContentPane().add(speedrunPanel, "speedrun");
+        frame.getContentPane().add(endPanel, "end");
+    }
 
-	public EndPanel getEndPanel() {
-		return endPanel;
-	}
+    @Override
+    public void mousePressed(MouseEvent e) { // mouseClicked로 변경가능
+        if (e.getComponent().toString().contains("IntroPanel")) { // IntroPanel에서 마우스를 눌렀다면
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            cl.show(frame.getContentPane(), "select"); // select패널을 카드레이아웃 최상단으로 변경
+            selectPanel.requestFocus(); // 리스너를 select패널에 강제로 줌
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Main window = new Main();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}  
+        } else if (e.getComponent().getName().equals("StartBtn")) { // StartBtn이라는 이름을 가진 버튼을 눌렀다면
+            if (selectPanel.getCi() == null) {
+                JOptionPane.showMessageDialog(null, "캐릭터를 골라주세요"); // 캐릭터를 안골랐을경우 팝업
+            } else {
+                cl.show(frame.getContentPane(), "game"); // 캐릭터를 골랐다면 게임패널을 카드레이아웃 최상단으로 변경
+                gamePanel.gameSet(selectPanel.getCi()); // 쿠키이미지를 넘겨주고 게임패널 세팅
+                gamePanel.gameStart(); // 게임시작
+                gamePanel.requestFocus(); // 리스너를 game패널에 강제로 줌
+            }
 
-	public Main() {
-		initialize();
-	}
+        } else if (e.getComponent().getName().equals("SpeedrunBtn")) {
+            if (selectPanel.getCi() == null) {
+                JOptionPane.showMessageDialog(null, "캐릭터를 골라주세요"); // 캐릭터를 안골랐을경우 팝업
+            } else {
+                cl.show(frame.getContentPane(), "speedrun"); // 게임패널을 카드레이아웃 최상단으로 변경
+                speedrunPanel.gameSet(selectPanel.getCi()); // 쿠키이미지를 넘겨주고 게임패널 세팅
+                speedrunPanel.gameStart(); // 게임시작
+                speedrunPanel.requestFocus(); // 리스너를 game패널에 강제로 줌
+            }
+        } else if (e.getComponent().getName().equals("endAccept")) { // endAccept 이라는 이름을 가진 버튼을 눌렀다면
+            frame.getContentPane().remove(gamePanel); // 방금 했던 게임 패널을 프레임에서 삭제
+            gamePanel = new GamePanel(frame, cl, this); // 게임패널을 새 패널로 교체
+            gamePanel.setLayout(null);
+            frame.getContentPane().add(gamePanel, "game"); // 프레임에 새 게임패널 추가(카드레이아웃 하단)
 
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 500); // 창 사이즈 (100,100좌표는 아래의 frame.setLocationRelativeTo(null) 때문에 의미가 없어진다)
-		frame.setLocationRelativeTo(null); // 창을 화면 중앙에 배치
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 엑스버튼을 누르면 종료
-		cl = new CardLayout(0, 0); // 카드레이아웃 객체 생성
-		frame.getContentPane().setLayout(cl); // 프레임을 카드레이아웃으로 변경
-
-		introPanel = new IntroPanel();
-		introPanel.addMouseListener(this); // intro패널은 여기서 바로 넣는 방식으로 마우스리스너를 추가함.
-
-		storePanel = new StorePanel(this); // 상점
-
-		selectPanel = new SelectPanel(this); // Main의 리스너를 넣기위한 this
-		gamePanel = new GamePanel(frame, cl, this); // Main의 프레임 및 카드레이아웃을 이용하고 리스너를 넣기위한 this
-		endPanel = new EndPanel(this); // Main의 리스너를 넣기위한 this
-		speedrunPanel = new SpeedrunPanel(frame, cl, this); // 스피드런 모드 패널 추가
-
-		healthPotion = new HealthPotion();
-
-		// 모든 패널의 레이아웃을 null로 변환
-		introPanel.setLayout(null);
-		storePanel.setLayout(null);
-		selectPanel.setLayout(null);
-		gamePanel.setLayout(null);
-		endPanel.setLayout(null);
-
-		// 프레임에 패널들을 추가한다.(카드 레이아웃을 위한 패널들)
-		frame.getContentPane().add(introPanel, "intro");
-		frame.getContentPane().add(storePanel, "store");
-		frame.getContentPane().add(selectPanel, "select");
-		frame.getContentPane().add(gamePanel, "game");
-		frame.getContentPane().add(speedrunPanel, "speedrun");
-		frame.getContentPane().add(endPanel, "end");
-
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) { // mouseClicked로 변경가능
-		if (e.getComponent().toString().contains("IntroPanel")) { // IntroPanel에서 마우스를 눌렀다면
-			try {
-				Thread.sleep(300);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
-			}
-			cl.show(frame.getContentPane(), "select"); // select패널을 카드레이아웃 최상단으로 변경
-			selectPanel.requestFocus(); // 리스너를 select패널에 강제로 줌
-
-		} else if (e.getComponent().getName().equals("StartBtn")) { // StartBtn이라는 이름을 가진 버튼을 눌렀다면
-			if (selectPanel.getCi() == null) {
-				JOptionPane.showMessageDialog(null, "캐릭터를 골라주세요"); // 캐릭터를 안골랐을경우 팝업
-			} else {
-				cl.show(frame.getContentPane(), "game"); // 캐릭터를 골랐다면 게임패널을 카드레이아웃 최상단으로 변경
-				gamePanel.gameSet(selectPanel.getCi()); // 쿠키이미지를 넘겨주고 게임패널 세팅
-				gamePanel.gameStart(); // 게임시작
-				gamePanel.requestFocus(); // 리스너를 game패널에 강제로 줌
-			}
-
-		} else if (e.getComponent().getName().equals("SpeedrunBtn")) {
-			if (selectPanel.getCi() == null) {
-				JOptionPane.showMessageDialog(null, "캐릭터를 골라주세요"); // 캐릭터를 안골랐을경우 팝업
-			} else {
-				cl.show(frame.getContentPane(), "speedrun"); // 게임패널을 카드레이아웃 최상단으로 변경
-				speedrunPanel.gameSet(selectPanel.getCi()); // 쿠키이미지를 넘겨주고 게임패널 세팅
-				speedrunPanel.gameStart(); // 게임시작
-				speedrunPanel.requestFocus(); // 리스너를 game패널에 강제로 줌
-			}
-		} else if (e.getComponent().getName().equals("endAccept")) { // endAccept 이라는 이름을 가진 버튼을 눌렀다면
-			frame.getContentPane().remove(gamePanel); // 방금 했던 게임 패널을 프레임에서 삭제
-			gamePanel = new GamePanel(frame, cl, this); // 게임패널을 새 패널로 교체
-			gamePanel.setLayout(null);
-			frame.getContentPane().add(gamePanel, "game"); // 프레임에 새 게임패널 추가(카드레이아웃 하단)
-
-			frame.getContentPane().remove(selectPanel); // 방금 선택했던 select패널을 삭제
-			selectPanel = new SelectPanel(this); // select 패널을 새 패널로 교체
-			selectPanel.setLayout(null);
-			frame.getContentPane().add(selectPanel, "select"); // 프레임에 새 select패널 추가(카드레이아웃 하단)
-			cl.show(frame.getContentPane(), "select"); // 새 select패널을 카드레이아웃 최상단으로 이동 (화면에 보임)
-			selectPanel.requestFocus(); // 리스너를 select패널에 강제로 줌
-		} else if (e.getComponent().getName().equals("StoreBtn")) { // StoreBtn 을 눌렀다면
-			cl.show(frame.getContentPane(), "store");
-			storePanel.requestFocus();
-		} else if (e.getComponent().getName().equals("HealthPotionBtn")) {
-			cl.show(frame.getContentPane(), "healthpotion");
-			healthPotion.use(); // 추가 기능 구현 필요
-		}
-	}
+            frame.getContentPane().remove(selectPanel); // 방금 선택했던 select패널을 삭제
+            selectPanel = new SelectPanel(this); // select 패널을 새 패널로 교체
+            selectPanel.setLayout(null);
+            frame.getContentPane().add(selectPanel, "select"); // 프레임에 새 select패널 추가(카드레이아웃 하단)
+            cl.show(frame.getContentPane(), "select"); // 새 select패널을 카드레이아웃 최상단으로 이동 (화면에 보임)
+            selectPanel.requestFocus(); // 리스너를 select패널에 강제로 줌
+        } else if (e.getComponent().getName().equals("StoreBtn")) { // StoreBtn 을 눌렀다면
+            cl.show(frame.getContentPane(), "store");
+            storePanel.requestFocus();
+        } else if (e.getComponent().getName().equals("HealthPotionBtn")) {
+            cl.show(frame.getContentPane(), "healthpotion");
+            healthPotion.use(); // 추가 기능 구현 필요
+        }
+    }
 }
--- src/panels/IntroPanel.java
+++ src/panels/IntroPanel.java
@@ -1,48 +1,60 @@
 package panels;
 
-import java.awt.CardLayout;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-// import javafx.scene.image.Image;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class IntroPanel extends JPanel {
-	
-	ImageIcon introIc = new ImageIcon("img/intro/intro.png"); // 인트로 이미지
-	private ImageIcon storeIcon = new ImageIcon("img/store/storeBtn.png"); // 상점 버튼(수정 예정)
-	private JButton storeBtn;
+    private ImageIcon introIc = new ImageIcon("img/intro/intro.png"); // 인트로 이미지
+    private ImageIcon storeIcon = new ImageIcon("img/store/storeBtn.png"); // 상점 버튼(수정 예정)
+    private JButton storeBtn;
+    private boolean imageToggled = false; // 이미지 상태를 저장하는 플래그
+    private JFrame mainFrame;
+    private CardLayout cardLayout;
 
-	public IntroPanel() {
+    public IntroPanel(JFrame mainFrame, CardLayout cardLayout) {
+        this.mainFrame = mainFrame;
+        this.cardLayout = cardLayout;
         initialize();
     }
 
-	private void initialize() {
+    private void initialize() {
         setLayout(null); // 절대 위치 지정을 위해 레이아웃을 null로 설정
 
-		ImageIcon originalIcon = new ImageIcon("img/store/storeBtn.png"); // 원본 이미지
+        ImageIcon originalIcon = new ImageIcon("img/store/storeBtn.png"); // 원본 이미지
         Image originalImage = originalIcon.getImage(); // Image 객체로 변환
         Image scaledImage = originalImage.getScaledInstance(130, 40, Image.SCALE_SMOOTH); // 원하는 크기로 조정
         ImageIcon storeIcon = new ImageIcon(scaledImage); // 다시 ImageIcon으로 변환
-  
 
         storeBtn = new JButton(storeIcon);
         storeBtn.setName("StoreBtn"); // 버튼의 이름을 설정하여 식별 가능하도록 함
         storeBtn.setBounds(640, 80, 130, 40); // 버튼의 위치와 크기 설정
+        storeBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                toggleImage();
+                cardLayout.show(mainFrame.getContentPane(), "store");
+            }
+        });
         add(storeBtn); // 패널에 버튼 추가
     }
 
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g); // 화면을 비운다
-		
-		// 인트로 화면을 그린다
-		g.drawImage(introIc.getImage(), -60, 0, /* this.getWidth(), this.getHeight(), */ null);
-	}
+    private void toggleImage() {
+        if (imageToggled) {
+            introIc = new ImageIcon("img/intro/intro.png"); // 원래 인트로 이미지로 변경
+        } else {
+            introIc = new ImageIcon("img/store/storePanel.png"); // 두 번째 이미지로 변경
+        }
+        imageToggled = !imageToggled; // 이미지 상태 플래그 토글
+        repaint(); // 패널 다시 그리기
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); // 화면을 비운다
+
+        // 인트로 화면을 그린다
+        g.drawImage(introIc.getImage(), -60, 0, /* this.getWidth(), this.getHeight(), */ null);
+    }
 }
